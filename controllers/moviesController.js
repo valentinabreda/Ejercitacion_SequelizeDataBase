1298e7d26c422276130251b007b194f3528e61c4
valentinabreda <valentinabreda.s@gmail.com>
2020-06-14 18:49:24 -0300

    finalizado

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -2,19 +2,77 @@ const db = require('../models')
 
 
 let moviesController = {
-    listado: function(req,res){
+    listado: (req,res) => {
         db.Peliculas.findAll()
         .then(function(peliculas){
             res.render('movies', {peliculas:peliculas})
+        }).catch( (error) => {
+            return res.send('Ocurrió un error');
         });
     },
-    detalle: function(req, res){
+
+    detalle: (req, res) => {
         db.Peliculas.findByPk(req.params.id)
-        .then(function(pelicula) {
-            res.render('moviesdetail', {pelicula:pelicula})
+        .then(function(peliculas) {
+            res.render('moviesdetail', {peliculas:peliculas})
+        }).catch( (error) => {
+            return res.send('Ocurrió un error');
+        });
+    },
+
+    ultimasCinco: (req, res) => {
+        db.Peliculas.findAll({
+            order: [
+                ['release_date', 'DESC']
+            ],
+            limit: 5
+        })
+        .then(function(peliculas) {
+            res.render('ultimasCinco', {peliculas: peliculas})
+        }).catch( (error) => {
+            return res.send('Ocurrió un error');
+        });
+    },
+
+    recomendadas: (req, res) => {
+        db.Peliculas.findAll({
+            where: {
+                awards: {[db.Sequelize.Op.gte]: 8}
+            }
+        }).then( (peliculas) => {
+            return res.render('recomendadas', {peliculas: peliculas});
+        }).catch( (error) => {
+            return res.send('Ocurrió un error');
+        });
+    },
+
+    formularioBusqueda: (req, res) => {
+        return res.render('buscarPeliculas', {
+            title: 'Buscador'
+        });
+    },
+
+    buscar: (req, res) => {
+        const busqueda = req.body.buscar;
+        db.Peliculas.findAll({
+            where:{
+                title: {[db.Sequelize.Op.like]: `%${busqueda}%`}
+            }
+        }).then( (peliculas) => {
+            if(peliculas.length == 0) {
+                return res.send('No se encontraron resultados para su busqueda');
+            };
+            return res.render('resultadoPeliculaBuscada',{
+                title: 'Peliculas Encontradas',
+                peliculas: peliculas
+            });
+        }).catch( (error) => {
+            return res.send('Ocurrió un error');
         });
     }
-}
+};
+    
+
 
 
 
--- routes/movies.js
+++ routes/movies.js
@@ -4,6 +4,11 @@ var moviesController = require('../controllers/moviesController');
 
 router.get('/', moviesController.listado);
 router.get('/detail/:id', moviesController.detalle);
+router.get('/new', moviesController.ultimasCinco);
+router.get('/recommended', moviesController.recomendadas);
+router.get('/search', moviesController.formularioBusqueda);
+router.post('/search', moviesController.buscar);
+
 
 
 
